0693cf7fea237547ba7bd81de0e97d7917dbe18b
Nils Feege <nils.feege@stonybrook.edu>
2017-05-11 15:45:32 -0400

    Global constants

---
+++ solid_eic_evgen/JPsi/globals.h
@@ -0,0 +1,13 @@
+/* gloabl variables */
+namespace simglobals
+{
+  const Double_t DEG = 180./3.1415926;
+
+  const Double_t mass_e = 0.511e-3;
+  const Double_t mass_p = 0.93827;
+  // const Double_t mass_n = 0.939566;
+  const Double_t mass_jpsi = 3.097;
+  // const Double_t width_jpsi = 93.2e-6;
+  const Double_t mass_upsilon = 9.460;
+  // const Double_t width_upsilon = 54.0e-6;
+}
