8ae960224ed3cfdff5e8767e4edf872c0f1899d6
Nils Feege <nils.feege@stonybrook.edu>
2017-05-11 23:37:11 -0400

    Update input files

--- analysis/plot_compare_codes.C
+++ analysis/plot_compare_codes.C
@@ -1,26 +1,26 @@
 int plot_compare_codes()
 {
 
-  TFile *fin1 = new TFile("../data/sim_te_b5GeV_p50GeV_mjpsi.root","OPEN");
+  TFile *fin1 = new TFile("../solid_evgen/JPsi/sim_te_b11GeV_p0GeV_mjpsi.root","OPEN");
   TTree *T1 = (TTree*)fin1->Get("T");
 
-  TFile *fin2 = new TFile("../data/sim_te_b5GeV_p50GeV_mjpsi.root","OPEN");
+  TFile *fin2 = new TFile("../solid_eic_evgen/JPsi/sim_te_b11GeV_p0GeV_mjpsi.root","OPEN");
   TTree *T2 = (TTree*)fin2->Get("T");
 
   //  gROOT->SetStyle("Plain");
   gStyle->SetOptStat(0);
 
-  // Pseudorapidity of J/Psi
+  // Theta angle of J/Psi
   TCanvas *c1 = new TCanvas();
-  TH1F* h1_1 = new TH1F("h1_1","J/Psi pseudorapiditiy;# entries;#eta^{J/#Psi}",100,-10,10);
+  TH1F* h1_1 = new TH1F("h1_1","J/Psi theta angle;# entries;#theta^{J/#Psi}",100,0,10);
   TH1F* h1_2 = (TH1F*)h1_1->Clone("h1_2");
 
   h1_1->SetLineColor(kBlue);
   h1_2->SetLineColor(kOrange+2);
   h1_2->SetLineStyle(2);
 
-  T1->Draw("eta_jpsi >> h1_1");
-  T2->Draw("eta_jpsi >> h1_2");
+  T1->Draw("theta_jpsi >> h1_1");
+  T2->Draw("theta_jpsi >> h1_2");
 
   h1_1->Draw();
   h1_2->Draw("same");
@@ -30,7 +30,7 @@ int plot_compare_codes()
 
   // Total momentum of J/Psi
   TCanvas *c2 = new TCanvas();
-  TH1F* h2_1 = new TH1F("h2_1","J/Psi pseudorapiditiy;# entries;p^{J/#Psi}",100,0,55);
+  TH1F* h2_1 = new TH1F("h2_1","J/Psi total momentum;# entries;p^{J/#Psi}",150,0,15);
   TH1F* h2_2 = (TH1F*)h2_1->Clone("h2_2");
 
   h2_1->SetLineColor(kBlue);
