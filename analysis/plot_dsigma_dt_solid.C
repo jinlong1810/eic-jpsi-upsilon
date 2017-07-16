c8295b4d814dc31198acb1791468562669f55fae
Nils Feege <nils.feege@stonybrook.edu>
2017-07-16 11:59:12 -0400

    Switch to plotting number of entries

--- analysis/plot_dsigma_dt_solid.C
+++ analysis/plot_dsigma_dt_solid.C
@@ -2,7 +2,6 @@
 
 int plot_dsigma_dt_solid()
 {
-  //TFile *fin_11GeV = new TFile("../data/sim_te_b11GeV_p0GeV_mjpsi_10k_accep.root","OPEN");
   TFile *fin_11GeV = new TFile("../data/sim_te_b11GeV_p0GeV_mjpsi_v2_accep.root","OPEN");
   TTree *T = (TTree*)fin_11GeV->Get("T");
 
@@ -12,6 +11,7 @@ int plot_dsigma_dt_solid()
   /* get normalization */
   Double_t overall = get_norm_solid_overall(T);
 
+
   /* define acceptance */
   char accep_normal[200];
   sprintf(accep_normal,"%s","(accep_je1_1+accep_je1_2)*(accep_je2_1+accep_je2_2)*(accep_e_1)*(accep_p_1+accep_p_2)");
@@ -19,12 +19,12 @@ int plot_dsigma_dt_solid()
   TH1F* count_dsigma_dt = new TH1F("count_dsigma_dt","",30,0,3);
   count_dsigma_dt->SetMarkerStyle(20);
   count_dsigma_dt->SetMarkerColor(kRed+1);
-  count_dsigma_dt->GetYaxis()->SetTitle("#frac{d#sigma}{dt} (nb/GeV^{2})");
+  count_dsigma_dt->GetYaxis()->SetTitle("# events");//#frac{d#sigma}{dt} (nb/GeV^{2})");
   count_dsigma_dt->GetXaxis()->SetTitle("|t-t_{min}| (GeV^{2})");
 
-  const unsigned nbins_Keq = 4;
-  float a_Keqmin[nbins_Keq] = {9.09, 9.29, 9.49, 9.69};
-  float a_Keqmax[nbins_Keq] = {9.29, 9.49, 9.69, 9.89};
+  const unsigned nbins_Keq = 5;
+  float a_Keqmin[nbins_Keq] = {9.09, 9.29, 9.49, 9.69, 0};
+  float a_Keqmax[nbins_Keq] = {9.29, 9.49, 9.69, 9.89, 1000};
 
   for ( unsigned i = 0; i < nbins_Keq; i++ )
     {
@@ -40,7 +40,7 @@ int plot_dsigma_dt_solid()
 
       cout << "Entries: " << count_dsigma_dt->GetEntries() << endl;
 
-      count_dsigma_dt->DrawClone("EP");
+      count_dsigma_dt->DrawClone("");
       c1->SetLogx(0);
       c1->SetLogy(1);
       c1->Print(Form("plots/solid_dsigma_dt_KeqBin_%.2f_%.2f.png", Keqmin, Keqmax) );
